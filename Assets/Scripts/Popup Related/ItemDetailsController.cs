829505272a66da5b4522c7f50ba82ecb0a390241
Ashwin02 <ashwinabraham2001@gmail.com>
2025-06-13 17:30:19 +0530

    Formatted ItemDetailsController script

--- Assets/Scripts/Popup Related/ItemDetailsController.cs
+++ Assets/Scripts/Popup Related/ItemDetailsController.cs
@@ -9,7 +9,6 @@ public class ItemDetailsController : MonoBehaviour
 
     //data required for ItemDetailsController
     private ItemContext itemContext;
-    //private ItemModel itemModel;//this is being replaced by ItemScriptableObject itemDisplayDetails
     private ItemScriptableObject itemDetails;
 
     //reference to components
@@ -21,27 +20,17 @@ public class ItemDetailsController : MonoBehaviour
     [SerializeField] private TextMeshProUGUI itemPriceText;
     [SerializeField] private TextMeshProUGUI itemQuantityText;
     [SerializeField] private TextMeshProUGUI itemWeightText;
-
     [SerializeField] private Button actionButton;
 
-    public void Init(ItemCountSetterController itemCountSetterController)
-    {
-        this.itemCountSetterController = itemCountSetterController;
-    }
+    public void Init(ItemCountSetterController itemCountSetterController) => this.itemCountSetterController = itemCountSetterController;
 
-    private void OnEnable()
-    {
-        actionButton.onClick.AddListener(() => OnActionButtonClicked(itemDetails, itemContext));//previously first paramter was ItemModel
-    }
-    private void OnDisable()
-    {
-        actionButton.onClick.RemoveAllListeners();
-    }
+    private void OnEnable() => actionButton.onClick.AddListener(() => OnActionButtonClicked(itemDetails, itemContext));
 
-    public void UpdateDetails(ItemScriptableObject itemSO, ItemContext _context)//previously first paramter was ItemModel
+    private void OnDisable() => actionButton.onClick.RemoveAllListeners();
+
+    public void UpdateDetails(ItemScriptableObject itemSO, ItemContext itemContext)
     {
-        itemContext = _context;
-        //itemModel = _model;
+        this.itemContext = itemContext;
         this.itemDetails = itemSO;
 
         itemNameText.text = "Item Name : " + itemDetails.id.ToString();
@@ -49,13 +38,12 @@ public class ItemDetailsController : MonoBehaviour
         itemIconImage.sprite = itemDetails.icon;
         itemTypeText.text = "Item Type : " + itemDetails.type.ToString();
         itemRarityText.text = "Item Rarity : " + itemDetails.rarity.ToString();
-        Debug.Log(itemDetails.rarity);
         itemWeightText.text = "Item Weight : " + itemDetails.weight.ToString();
         itemQuantityText.text = "Item Quantity : " + itemDetails.quantity.ToString();
 
-        actionButton.GetComponentInChildren<TextMeshProUGUI>().text = _context.ToString();
+        actionButton.GetComponentInChildren<TextMeshProUGUI>().text = itemContext.ToString();
 
-        switch (_context)
+        switch (itemContext)
         {
             case ItemContext.Buy:
                 itemPriceText.text = "Item Buying Price : " + itemDetails.buyingPrice;
@@ -65,9 +53,10 @@ public class ItemDetailsController : MonoBehaviour
                 break;
         }
     }
-    private void OnActionButtonClicked(ItemScriptableObject itemSO, ItemContext _context)//previously first parameter was ItemModel
+
+    private void OnActionButtonClicked(ItemScriptableObject itemSO, ItemContext _context)
     {
         itemCountSetterController.gameObject.SetActive(true);
-        itemCountSetterController.UpdateDetails(itemSO, _context);//previously first paramter was ItemModel
+        itemCountSetterController.UpdateDetails(itemSO, _context);
     }
 }
